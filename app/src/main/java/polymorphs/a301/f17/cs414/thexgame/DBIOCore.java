0445b362bbd58e6d76faf27a65affbf413e22cbc
Miles Blaisus <milesblaisus@gmail.com>
2017-10-14 21:26:23 -0600

    Adds very rough draft of database interactions DO NOT MERGE TO MASTER.
    This is just to give you all some ideas on how to use the database. Look at the last lines of onCreate to see how to attach listeners and the top of openNotificationsFragment to see how to add to a list. The stall line in openCurrentGames is to inspect and verify the data update via listeners. Also the DBIOCore has been updated to closer resemble it's final form.

--- app/src/main/java/polymorphs/a301/f17/cs414/thexgame/DBIOCore.java
+++ app/src/main/java/polymorphs/a301/f17/cs414/thexgame/DBIOCore.java
@@ -3,13 +3,12 @@ package polymorphs.a301.f17.cs414.thexgame;
 
 import android.util.Log;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.io.Serializable;
 
 import static android.content.ContentValues.TAG;
 
@@ -18,66 +17,50 @@ import static android.content.ContentValues.TAG;
  */
 
 
-public class DBIOCore implements Serializable {
+public class DBIOCore {
+    private static DatabaseReference baseReference = FirebaseDatabase.getInstance().getReference();
+    private static String currentUser;
+    private static Boolean newUser = false;
 
-    Invitation outputInvite; // Invitation is Serializable, but we also need to make DBIOCore serializable as well.
-
-    public DBIOCore() {
-
-    }
-
-    // Added by Roger to test the serializable creation of the database.
-    public String testDatabaseCreation()
-    {
-        return "Database was created successfully!";
-    }
-
-    public void runExperimentSetWatch() {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("testInvite1");
-
-        ValueEventListener inviteListener = new ValueEventListener() {
+    public static void setCurrentUser(String userName) {
+        currentUser = userName;
+        DatabaseReference tmp = getUserReference();
+        tmp.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // Get Post object and use the values to update the UI
-                outputInvite = dataSnapshot.getValue(Invitation.class);
+                User testUser = dataSnapshot.getValue(User.class);
+                if (testUser == null) {
+                    getUserReference().setValue(new User(currentUser));
+                }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                // Getting Post failed, log a message
                 Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
             }
-        };
-
-        myRef.addValueEventListener(inviteListener);
+        });
     }
 
-    public void runExperimentSendData() {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("testInvite1");
-        Invitation invite = new Invitation("Miles", "Andy");
-        myRef.setValue(invite);
+
+    public static DatabaseReference getUserReference() {
+        return baseReference.child("users").child(currentUser);
     }
 
-    public void runExperimentGetData() {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("testInvite1");
-
-        ValueEventListener inviteListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // Get Post object and use the values to update the UI
-                outputInvite = dataSnapshot.getValue(Invitation.class);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                // Getting Post failed, log a message
-                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
-            }
-        };
-
-        myRef.addListenerForSingleValueEvent(inviteListener);
+    public static DatabaseReference getInvitationsReference() {
+        return baseReference.child("invitations").child(currentUser);
     }
+
+//    ValueEventListener inviteListener =  new ValueEventListener() {
+//        @Override
+//        public void onDataChange(DataSnapshot dataSnapshot) {
+//            // Get Post object and use the values to update the UI
+//            outputInvite = dataSnapshot.getValue(Invitation.class);
+//        }
+//
+//        @Override
+//        public void onCancelled(DatabaseError databaseError) {
+//            // Getting Post failed, log a message
+//            Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+//        }
+//    };
 }
